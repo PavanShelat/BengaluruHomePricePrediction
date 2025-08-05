0f723c670eb12c6464dbe7ac0aee38902760d746
PavanShelat <114757146+PavanShelat@users.noreply.github.com>
2025-08-05 13:32:10 +0530

    Updated app.js with correct backend URL

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 function getBathValue() {
   var uiBathrooms = document.getElementsByName("uiBathrooms");
-  for(var i in uiBathrooms) {
-    if(uiBathrooms[i].checked) {
-        return parseInt(i)+1;
+  for (var i in uiBathrooms) {
+    if (uiBathrooms[i].checked) {
+      return parseInt(i) + 1;
     }
   }
   return -1; // Invalid Value
@@ -10,9 +10,9 @@ function getBathValue() {
 
 function getBHKValue() {
   var uiBHK = document.getElementsByName("uiBHK");
-  for(var i in uiBHK) {
-    if(uiBHK[i].checked) {
-        return parseInt(i)+1;
+  for (var i in uiBHK) {
+    if (uiBHK[i].checked) {
+      return parseInt(i) + 1;
     }
   }
   return -1; // Invalid Value
@@ -20,42 +20,43 @@ function getBHKValue() {
 
 function onClickedEstimatePrice() {
   console.log("Estimate price button clicked");
+
   var sqft = document.getElementById("uiSqft");
   var bhk = getBHKValue();
   var bathrooms = getBathValue();
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "http://127.0.0.1:5000/predict_home_price"; // Use this for local Flask server
+  var url = "https://bengaluru-home-price-prediction-j4k0.onrender.com/predict_home_price"; // ✅ Render URL
 
   $.post(url, {
-      total_sqft: parseFloat(sqft.value),
-      bhk: bhk,
-      bath: bathrooms,
-      location: location.value
-  },function(data, status) {
-      console.log(data.estimated_price);
-      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
-      console.log(status);
+    total_sqft: parseFloat(sqft.value),
+    bhk: bhk,
+    bath: bathrooms,
+    location: location.value
+  }, function (data, status) {
+    console.log(data.estimated_price);
+    estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+    console.log(status);
   });
 }
 
 function onPageLoad() {
-  console.log( "document loaded" );
-  // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "http://127.0.0.1:5000/get_location_names"; // Use this for local Flask server
-  $.get(url,function(data, status) {
-      console.log("got response for get_location_names request");
-      if(data) {
-          var locations = data.locations;
-          var uiLocations = document.getElementById("uiLocations");
-          $('#uiLocations').empty();
-          for(var i in locations) {
-              var opt = new Option(locations[i]);
-              $('#uiLocations').append(opt);
-          }
+  console.log("document loaded");
+
+  var url = "https://bengaluru-home-price-prediction-j4k0.onrender.com/get_location_names"; // ✅ Render URL
+
+  $.get(url, function (data, status) {
+    console.log("got response for get_location_names request");
+    if (data) {
+      var locations = data.locations;
+      var uiLocations = document.getElementById("uiLocations");
+      $('#uiLocations').empty();
+      for (var i in locations) {
+        var opt = new Option(locations[i]);
+        $('#uiLocations').append(opt);
       }
+    }
   });
 }
 
